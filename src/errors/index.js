7c23aace4bc5a48c6eb71e539350f08a9abad864
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-02 16:25:12 -0500

    Adding not found exception

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,7 +1,9 @@
 import BadRequest from './BadRequest.js';
 import Conflict from './Conflict.js';
+import NotFound from './NotFound.js';
 
 export default {
   BadRequest,
   Conflict,
+  NotFound,
 };
