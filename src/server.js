4a44b875f164d40794b269768646203970b20ccf
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-02 09:24:16 -0500

    Initial express project

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+const port = 3001
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    data: 'It works!'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
