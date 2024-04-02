1f9d96e369d94a388f159c9aab8acef67c50d9b2
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-02 09:57:28 -0500

    Fixing test with new URL and response structure

--- src/tests/groups.test.js
+++ src/tests/groups.test.js
@@ -3,14 +3,13 @@ import supertest from "supertest"
 
 const requestWithSupertest = supertest(server)
 
-describe('Base endpoint', () => {
-  it('GET / should return it works message', async () => {
-    const res = await requestWithSupertest.get('/')
+describe('Groups endpoints', () => {
+  it('GET /api/groups should return all groups', async () => {
+    const res = await requestWithSupertest.get('/api/groups')
 
     expect(res.status).toEqual(200)
     expect(res.type).toEqual(expect.stringContaining('json'))
     expect(res.body).toBeDefined()
     expect(res.body.data).toBeDefined()
-    expect(res.body.data).toBe('It works!')
   })
 })
