9bedc7334c8b541e54b9b4ba2d578591e86ab2d9
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-04-02 14:41:32 -0500

    Database abstraction class created

---
+++ src/bd/Database.js
@@ -0,0 +1,39 @@
+class Database {
+  constructor() {
+    this.entities = [];
+  }
+
+  getAll() {
+    return this.entities;
+  }
+
+  getById(id) {
+    return this.entities.find((element) => element.id === Number(id));
+  }
+
+  getByProp(prop, value) {
+    return this.entities.find((element) => element[prop] === value);
+  }
+
+  save(entity) {
+    const generatedId = (this.entities[this.entities.length - 1] || { id: 0 }).id + 1;
+    this.entities.push({
+      ...entity,
+      id: generatedId,
+    });
+  }
+
+  delete(id) {
+    return this.entities.filter((entity) => entity.id !== id);
+  }
+
+  update(id, newEntity) {
+    const entityIndex = this.entities.findIndex((entity) => entity.id === id);
+
+    if (entityIndex) {
+      this.entities[entityIndex] = newEntity;
+    }
+  }
+}
+
+export default Database;
